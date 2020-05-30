37965fa26831cb7fe22c34f120c34aed75f4a114
psk907 <p.surajkumar907@gmail.com>
2020-05-30 16:59:33 +0530

    added names and logo

--- lib/components/moreoptions.dart
+++ lib/components/moreoptions.dart
@@ -1,4 +1,3 @@
-
 import 'package:crportal/services/authservice.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -9,94 +8,103 @@ class MoreOptions extends StatefulWidget {
 }
 
 class _MoreOptionsState extends State<MoreOptions> {
-  Choice _selectedChoice ;
+  Choice _selectedChoice;
 
   void _select(Choice choice) {
     setState(() {
       _selectedChoice = choice;
     });
   }
-
-  List<Choice> choices =  <Choice>[
   
-   
+  List<Choice> choices = <Choice>[
     Choice(
-      title: 'About', 
+      title: 'About',
       icon: Icons.info,
-      onPressed: (context){
-          showAboutDialog(context: context);
-        },
+      onPressed: (context) {
+        showAboutDialog(
+            context: context,
+            applicationIcon: CircleAvatar(
+              backgroundColor: Colors.deepPurple,
+              child: Icon(Icons.assignment, color: Colors.white),
+            ),
+            applicationName: "CR Portal",
+            children: <Widget>[
+              Text("Developed by"),
+              ListTile(contentPadding: EdgeInsets.all(1),title: Text("Aakash Pothepalli"),leading: IconButton(icon:Image.asset("assets/GitHublight.png"),),),
+              ListTile(contentPadding: EdgeInsets.all(1),title: Text("Suraj Kumar"),leading: IconButton(icon:Image.asset("assets/GitHublight.png"),),),
+            ]
+            );
+      },
     ),
     Choice(
-      title: 'View Source Code', 
+      title: 'View Source Code',
       icon: Icons.code,
-      onPressed: (context)async{
-          const url = 'https://github.com/teamimpracticalcoders/MSRIT-CR-Portal';
-            if (await canLaunch(url)) {
-              await launch(url);
-            } else {
-              throw 'Could not launch $url';
-            }
-        },
+      onPressed: (context) async {
+        const url = 'https://github.com/teamimpracticalcoders/MSRIT-CR-Portal';
+        if (await canLaunch(url)) {
+          await launch(url);
+        } else {
+          throw 'Could not launch $url';
+        }
+      },
     ),
-     Choice(
-      title: 'Sign Out', 
+    Choice(
+      title: 'Sign Out',
       icon: Icons.exit_to_app,
-      onPressed: (context){
-          AuthService().signOut();
-          Navigator.pushReplacement(
-              context,
-              new MaterialPageRoute(
-                  builder: (context) => AuthService().handleAuth()));
-        },
+      onPressed: (context) {
+        AuthService().signOut();
+        Navigator.pushReplacement(
+            context,
+            new MaterialPageRoute(
+                builder: (context) => AuthService().handleAuth()));
+      },
     ),
-    
   ];
-
 
   @override
   Widget build(BuildContext context) {
-    return 
-    PopupMenuButton<Choice>(
-        onSelected: _select,
-        itemBuilder: (BuildContext context) {
-          return choices.map((Choice choice) {
-            return PopupMenuItem<Choice>(
-              value: choice,
-              child: ChoiceCard(choice: choice,),
-            );
-          }).toList();
-        },
-      );
-          
-        
+    return PopupMenuButton<Choice>(
+      onSelected: _select,
+      itemBuilder: (BuildContext context) {
+        return choices.map((Choice choice) {
+          return PopupMenuItem<Choice>(
+            value: choice,
+            child: ChoiceCard(
+              choice: choice,
+            ),
+          );
+        }).toList();
+      },
+    );
   }
 }
 
 class Choice {
-  Choice({this.title, this.icon,this.onPressed});
-   String title;
-   IconData icon;
-   Function onPressed;
+  Choice({this.title, this.icon, this.onPressed});
+  String title;
+  IconData icon;
+  Function onPressed;
 }
-
 
 class ChoiceCard extends StatelessWidget {
   ChoiceCard({Key key, this.choice}) : super(key: key);
 
   Choice choice;
 
-        
   @override
   Widget build(BuildContext context) {
-    return  FlatButton(
-        child: Row(children: <Widget>[
-         Expanded(child: Text(choice.title,style: TextStyle(fontSize: 15),)),
-         Icon(choice.icon),
-        
-        ],),
-        onPressed: () => choice.onPressed(context),
+    return FlatButton(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: Text(
+            choice.title,
+            style: TextStyle(fontSize: 15),
+          )),
+          Icon(choice.icon),
+        ],
+      ),
+      onPressed: () => choice.onPressed(context),
     );
-    
   }
 }
